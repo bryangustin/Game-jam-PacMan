eb03fb9f0cfaf4d55d8403c64322ae1c7a3bbb0b
Valerian Thibault <valerianderenne@gmail.com>
2020-10-27 12:04:55 +0100

    Design for pacman and mouth moving. Was not able to make it move (neither the mouth or movement) while trying to add it with other elements...

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,6 +1,31 @@
 
+uppermouth = 1.85 // upper mouth radian
+lowermouth = 0.2 // lower mouth radian
+speed = 0.01 // speed of mouth opening & closing
+
 function setup() {
 }
 
 function draw() {
+
+ //Pacman body
+ fill(255, 255, 0);
+ ellipse(20, 20, 30, 30);
+ 
+ //Pacman mouth
+ fill(81, 194, 231);
+ // if the upper mouth radian reaches 2*PI, 
+ // the direction reverses.
+ // this affects the lower mouth as well.
+ if (i >= 2 || i <= 1.8) {
+   speed = speed*(-1) 
+ }
+ arc(20, 20, 30, 30, (i+=speed)*PI, (x-=speed)*PI, PIE);
+ 
+ //Pacman eye
+ ellipse(20, 12, 3, 3);
+ fill(81, 194, 231);
+ stroke(81, 194, 231);
+
+
 }
